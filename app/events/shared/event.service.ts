7310cc16a7e4e9c373b84ebda1f35ad037ba8215
Adam Heiderich <Damheid@users.noreply.github.com>
2017-04-24 18:09:50 +0200

    Add files via upload

--- app/events/shared/event.service.ts
+++ app/events/shared/event.service.ts
@@ -4,7 +4,12 @@ import { Injectable } from '@angular/core';
 export class EventService {
 	getEvents() {
 		return EVENTS;
-	}
+	};
+
+	getEvent (id:number) {
+		return EVENTS.find(event => event.id === id);
+	};
+
 }
 
 const EVENTS = [
