5ffdc7368dce17f9ce69d307bcb003e0ed69e56e
Adam Heiderich <Damheid@users.noreply.github.com>
2017-04-25 14:56:28 +0200

    Add files via upload

--- app/app.module.ts
+++ app/app.module.ts
@@ -4,28 +4,34 @@ import { RouterModule } from "@angular/router";
 
 // Components
 import { EventsAppComponent } from './events-app.component';
-import { EventsListComponent } from './events/events-list.component';
-import { EventThumbnailComponent } from './events/event-thumbnail.component';
-import { NavbarComponent } from './nav/navbar.component';
-import { EventDetailsComponent } from "./events/event-details/event-details.component";
-import { CreateEventComponent } from "./events/create-event.component";
+
+import {
+	EventsListComponent,
+	EventThumbnailComponent,
+	EventDetailsComponent,
+	CreateEventComponent
+} from './events/index';
+
+import { NavbarComponent } from './nav/index';
 
 // Errors
-import { Error404Component } from "./errors/404.component";
+import { Error404Component } from "./errors/index";
 
 // Services
 import { EventService } from './events/shared/event.service';
 import { ToastrService } from "./common/toastr.service";
+import { EventsListResolver } from "./events/events-list-resolver.service";
 
 // Router
 import { appRoutes } from "./routers";
 import { EventRouteActivator } from "./events/event-details/event-route-activator.service";
 
+
 @NgModule({
 	imports: [
 		BrowserModule,
 		RouterModule.forRoot(appRoutes)
-		],
+	],
 	declarations: [
 		EventsAppComponent,
 		EventsListComponent,
@@ -38,8 +44,20 @@ import { EventRouteActivator } from "./events/event-details/event-route-activato
 	providers: [
 		EventService,
 		ToastrService,
-		EventRouteActivator
-		],
+		EventRouteActivator,
+		EventsListResolver,
+		{
+			provide: 'canDeactivateCreateEvent',
+			useValue: checkDirtyState
+		}
+	],
 	bootstrap: [EventsAppComponent]
 })
 export class AppModule { }
+
+function checkDirtyState(component: CreateEventComponent) {
+	if (component.isDirty)
+		return window.confirm('You have not saved this event, do you really want to cancel?');
+
+	return true;
+}
